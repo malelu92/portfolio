ac9c725c3e8847a9f2e8de214fbf56f6f4daecc4
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-10-24 19:30:47 -0400

    created js file

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+$('a[href^="#"]').click(function () {
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+
+    return false;
+});
