bf9036129ed7f37744c40c36275670c6395d7a1c
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-11-16 15:02:05 -0500

    bar showing, but not for all

--- portfolio.js
+++ portfolio.js
@@ -1,29 +1,62 @@
 $(document).ready(function() {
 
- var shrinkHeader = 200;
+  var shrinkHeader = 200;
+
+  var sections = $('section');
+  var navbar = $('nav');
+  console.log("**** " + navbar.text())
+  var nav_height = navbar.outerHeight();
+
   $(window).scroll(function() {
+
+    /* shrink header */
     var scroll = getCurrentScroll();
-      if ( scroll >= shrinkHeader ) {
-           $('.header').addClass('shrink');
-           $('.name').addClass('shrink');
-           $('.nav-opt').addClass('shrink');
-           console.log("colocou shrink")
-        }
-        else {
-            $('.header').removeClass('shrink');
-            $('.name').removeClass('shrink');
-            $('.nav-opt').removeClass('shrink');
-            console.log("tirou shrink")
-        }
+    if ( scroll >= shrinkHeader ) {
+      $('.header').addClass('shrink');
+      $('.name').addClass('shrink');
+      $('.nav-opt').addClass('shrink');
+      console.log("colocou shrink")
+    }
+    else {
+      $('.header').removeClass('shrink');
+      $('.name').removeClass('shrink');
+      $('.nav-opt').removeClass('shrink');
+      console.log("tirou shrink")
+    }
+ 
+    /* bar under active link */
+    var cur_pos = $(this).scrollTop();
+ 
+    sections.each(function() {
+      console.log("entrou")
+      var top = $(this).offset().top - nav_height;
+      var bottom = top + $(this).outerHeight();
+ 
+  console.log(nav_height)
+      console.log("cur pos " + cur_pos)
+      console.log("top " + top)
+      console.log("bottom " + bottom)
+      if (cur_pos >= top && cur_pos <= bottom) {
+        navbar.find('a').removeClass('active');
+        sections.removeClass('active');
+ 
+        $(this).addClass('active');
+        console.log($(this).attr('id'))
+        navbar.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      }
+    });
   });
+});
+
 	function getCurrentScroll() {
     	return window.pageYOffset || document.documentElement.scrollTop;
     }
 
-  function onScroll(event) {
+/*  function onScroll(event) {
+    console.log("entrou")
   var sections = $('section');
   var navbar = $('.navbar');
-  var nav_height = nav.outerHeight();
+  var nav_height = navbar.outerHeight();
  
   var cur_pos = $(this).scrollTop();
  
@@ -32,14 +65,14 @@ $(document).ready(function() {
         bottom = top + $(this).outerHeight();
  
     if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
+      navbar.find('a').removeClass('active');
       sections.removeClass('active');
  
       $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      navbar.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
     }
   });
-}
+}*/
 
 
   /*function onScroll(event){
@@ -79,7 +112,3 @@ $(document).ready(function() {
       });
     } // End if
   });*/
-
-
-
-});
