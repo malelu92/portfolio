5a324fb9b9867a71a3bd29d8dd541b825a7a5936
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-11-16 16:31:28 -0500

    navbar working more or less

--- portfolio.js
+++ portfolio.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 
   var sections = $('section');
   var navbar = $('nav');
-  console.log("**** " + navbar.text())
   var nav_height = navbar.outerHeight();
 
   $(window).scroll(function() {
@@ -32,7 +31,6 @@ $(document).ready(function() {
       var top = $(this).offset().top - nav_height;
       var bottom = top + $(this).outerHeight();
  
-  console.log(nav_height)
       console.log("cur pos " + cur_pos)
       console.log("top " + top)
       console.log("bottom " + bottom)
@@ -51,64 +49,3 @@ $(document).ready(function() {
 	function getCurrentScroll() {
     	return window.pageYOffset || document.documentElement.scrollTop;
     }
-
-/*  function onScroll(event) {
-    console.log("entrou")
-  var sections = $('section');
-  var navbar = $('.navbar');
-  var nav_height = navbar.outerHeight();
- 
-  var cur_pos = $(this).scrollTop();
- 
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
- 
-    if (cur_pos >= top && cur_pos <= bottom) {
-      navbar.find('a').removeClass('active');
-      sections.removeClass('active');
- 
-      $(this).addClass('active');
-      navbar.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
-}*/
-
-
-  /*function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('#sidebar a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#sidebar ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
-}*/
-
-
-  /*$("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });*/
