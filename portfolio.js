39e5019215d0bb2b946ca184f73af67de00200b8
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-11-26 20:01:41 -0500

    idented switch function

--- portfolio.js
+++ portfolio.js
@@ -15,7 +15,7 @@ var state = "none";  // verticalscan, horizontalscan, none
 var interval = null;
 /*var keyboard = "off" //on*/
 var recentInputArea = null;
-var caps = "off" //on
+/*var caps = "off" //on*/
 var cont = 0;
 var topPage = 0;
 var leftPage = 0;
@@ -157,8 +157,7 @@ $(document).ready(function() {
 
   var shrinkHeader = 200;
   $(window).scroll(function() {
-
-    /* shrink header */
+    /* shrink header on scroll */
     var scroll = getCurrentScroll();
     if ( scroll >= shrinkHeader ) {
       $('.header').addClass('shrink');
@@ -181,30 +180,35 @@ function getCurrentScroll() {
   return window.pageYOffset || document.documentElement.scrollTop;
 }
 
+/* updates active menu option on navbar*/
 function scrollSection() {
   var nav_height = $('nav').outerHeight()+100;
   var sections = $('section');
-    var cur_pos = $(document).scrollTop();
-    sections.each(function () {
-        var top = $(this).offset().top - nav_height;
-        var bottom = top + $(this).outerHeight();
-        var sec_id = $(this).attr("id");
-        sec_id = sec_id.split('-')[1];
-        sec_id = "#" + sec_id + "-id";
-        if (cur_pos >= top && cur_pos <= bottom) {
-          /*console.log(sec_id)*/
-          var sec_id = $(this).attr("id");
-          sec_id = sec_id.split('-')[1];
-          sec_id = "#" + sec_id + "-id";
-          $(sec_id).addClass('active');
-        }
-        else {
-          $(sec_id).removeClass('active');
-        }
-    });
+  var cur_pos = $(document).scrollTop();
+
+  sections.each(function () {
+    var top = $(this).offset().top - nav_height;
+    var bottom = top + $(this).outerHeight();
+    var sec_id = $(this).attr("id");
+
+    sec_id = sec_id.split('-')[1];
+    sec_id = "#" + sec_id + "-id";
+
+    /*if curr position is on section add active mode*/
+    if (cur_pos >= top && cur_pos <= bottom) {
+      var sec_id = $(this).attr("id");
+      sec_id = sec_id.split('-')[1];
+      sec_id = "#" + sec_id + "-id";
+      $(sec_id).addClass('active');
+    }
+    else {
+      $(sec_id).removeClass('active');
+    }
+  });
 }
 
 //SWICTH INPUT ----------------
+/* Adds buttons that permit the user scroll the page up and down*/
 function addScrollButtons() {
   $("body").append("<input type='button' class='scroll down' value='down'>");
   $(".scroll.down").click(function() {
@@ -223,11 +227,13 @@ function addScrollButtons() {
   })
 }
 
+/* Removes buttons that permit the user scroll the page up and down*/
 function removeScrollButtons() {
   $(".scroll.down").remove();
   $(".scroll.up").remove();
 }
 
+/* Simulates clicks with single switch*/
 function simulateClick(element) {
   if (!element) return;
   var dispatchEvent = function (elt, name) {
@@ -242,152 +248,154 @@ function simulateClick(element) {
 };
 
 function switchInput(){
-        clearInterval(interval);
+  clearInterval(interval);
 
-      if(state=="none") {
+  if(state=="none") {
+    state = "verticalscan";
+    $("#horizontal-scanbar").css("top", 0+"px");
+    $("#horizontal-scanbar").show();
 
-        state = "verticalscan";
-        $("#horizontal-scanbar").css("top", 0+"px");
-        $("#horizontal-scanbar").show();
+    // Setting up the vertical scan
+    interval = setInterval(function() {
+      var offset = $("#horizontal-scanbar").offset();
+      var y = offset.top - topPage;
 
-        // Setting up the vertical scan
-        interval = setInterval(function() {
-          var offset = $("#horizontal-scanbar").offset();
-          var y = offset.top - topPage;
-
-          if(horizontalmovement=="down") {
-            console.log("somou 2")
-            y = y+2;
-          } else if(horizontalmovement=="up") {
-            y = y-2; 
-          }
-
-          if(y >= $(window).height()) {
-            horizontalmovement = "up";
-          } else if(y <= 0) {
-            horizontalmovement = "down";
-          }
-
-          console.log("new y is " + y + " " + $(window).height());
-
-          $("#horizontal-scanbar").css("top", y+"px");
-        }, 20);
-      } else if(state=="verticalscan") {
-        state = "horizontalscan";
-        $("#vertical-scanbar").css("left", 0+"px");
-        $("#vertical-scanbar").show();
-
-        // Setting up the vertical scan
-        interval = setInterval(function() {
-          var offset = $("#vertical-scanbar").offset();
-          var x = offset.left - leftPage;
-
-          if(verticalmovement=="right") {
-            x = x+2;
-          } else if(verticalmovement=="left") {
-            x = x-2;
-          }
-
-          if(x >= $(window).width()) {
-            verticalmovement = "left";
-          } else if(x <= 0) {
-            verticalmovement = "right";
-          }
-
-          console.log("new x is " + x + " " + $(window).width());
-
-          $("#vertical-scanbar").css("left", x+"px");
-        }, 20);
-      } 
-      else if(state=="horizontalscan") {
-        state = "none";
-        var offset = $("#vertical-scanbar").offset();
-        var x = offset.left - leftPage + $("#vertical-scanbar").width()/2.0;
-
-        var offset = $("#horizontal-scanbar").offset();
-        var y = offset.top - topPage + $("#horizontal-scanbar").height()/2.0;
-
-
-        $("body").append("<div class='click'></div>");
-
-        $(".click").css("left", x+"px");
-        $(".click").css("top", y+"px");
-
-        cont = 0;
-
-        $(".click").animate({
-          width: "+=25",
-          height: "+=25",
-          left: "-=12.5",
-          top: "-=12.5",
-          "border-radius": "+=12"
-        }, 800, function() {
-          if (cont == 0) {
-          $(".click").hide();
-          var elementtoclick = document.elementFromPoint(x, y);
-          simulateClick(elementtoclick);
-          console.log(elementtoclick)
-
-          if($(elementtoclick).is("input[type=\"text\"],textarea")) {
-            recentInputArea = elementtoclick;
-          }
-
-          if($(elementtoclick).attr('class') == "key letter") {
-            var letter = $.trim($(elementtoclick).text());
-            letter = String(letter);
-            if (caps == "off") { 
-              letter = letter.toString().toLowerCase();
-            }
-            $(recentInputArea).val($(recentInputArea).val() + letter);
-            previousClass = "key letter";
-          }
-
-          if($(elementtoclick).attr('class') == "key caps") {
-            if (caps == "off") {
-              caps = "on";
-            }
-            else {
-              caps = "off";
-            }
-            previousClass = "key caps";
-          }
-
-          if($(elementtoclick).attr('class') == "key backspace") {
-            var text = $(recentInputArea).val();
-            if (text.length > 1) {
-              text = text.slice(0, text.length - 1);
-            }
-            else {
-              text = "";
-            }
-            $(recentInputArea).val(text);
-            previousClass = "key backspace";
-          }
-
-          if($(elementtoclick).attr('class') == "key num dual") {
-            var num_dial = $.trim($(elementtoclick).text());
-            if(previousClass != "key shift left") {
-              var num = num_dial.slice(1,2)
-              $(recentInputArea).val($(recentInputArea).val() + num);
-            }
-            else {
-              var dial = num_dial.slice(0,1);
-              $(recentInputArea).val($(recentInputArea).val() + dial);
-            }
-            previousClass = "key num dual";
-          }
-
-          if ($(elementtoclick).attr('class') == "key shift left") {
-            previousClass = "key shift left";
-          }
-        }
-        cont +=1;
-
-        });
-
-        $("#horizontal-scanbar").hide();
-        $("#vertical-scanbar").hide();
+      if(horizontalmovement=="down") {
+        console.log("somou 2")
+        y = y+2;
       }
+      else if(horizontalmovement=="up") {
+        y = y-2; 
+      }
+
+      if(y >= $(window).height()) {
+        horizontalmovement = "up";
+      }
+      else if(y <= 0) {
+        horizontalmovement = "down";
+      }
+
+      console.log("new y is " + y + " " + $(window).height());
+
+      $("#horizontal-scanbar").css("top", y+"px");
+    }, 20);
+  }
+  else if(state=="verticalscan") {
+    state = "horizontalscan";
+    $("#vertical-scanbar").css("left", 0+"px");
+    $("#vertical-scanbar").show();
+
+    // Setting up the vertical scan
+    interval = setInterval(function() {
+      var offset = $("#vertical-scanbar").offset();
+      var x = offset.left - leftPage;
+
+      if(verticalmovement=="right") {
+        x = x+2;
+      }
+      else if(verticalmovement=="left") {
+        x = x-2;
+      }
+
+      if(x >= $(window).width()) {
+        verticalmovement = "left";
+      }
+      else if(x <= 0) {
+        verticalmovement = "right";
+      }
+
+      console.log("new x is " + x + " " + $(window).width());
+
+      $("#vertical-scanbar").css("left", x+"px");
+    }, 20);
+  } 
+  else if(state=="horizontalscan") {
+    state = "none";
+    var offset = $("#vertical-scanbar").offset();
+    var x = offset.left - leftPage + $("#vertical-scanbar").width()/2.0;
+
+    var offset = $("#horizontal-scanbar").offset();
+    var y = offset.top - topPage + $("#horizontal-scanbar").height()/2.0;
+
+    $("body").append("<div class='click'></div>");
+
+    $(".click").css("left", x+"px");
+    $(".click").css("top", y+"px");
+
+    cont = 0;
+
+    $(".click").animate({
+      width: "+=25",
+      height: "+=25",
+      left: "-=12.5",
+      top: "-=12.5",
+      "border-radius": "+=12"
+    }, 800, function() {
+      if (cont == 0) {
+        $(".click").hide();
+        var elementtoclick = document.elementFromPoint(x, y);
+        simulateClick(elementtoclick);
+        console.log(elementtoclick)
+
+        if($(elementtoclick).is("input[type=\"text\"],textarea")) {
+          recentInputArea = elementtoclick;
+        }
+
+        if($(elementtoclick).attr('class') == "key letter") {
+          var letter = $.trim($(elementtoclick).text());
+          letter = String(letter);
+          if (caps == "off") { 
+            letter = letter.toString().toLowerCase();
+          }
+          $(recentInputArea).val($(recentInputArea).val() + letter);
+          previousClass = "key letter";
+        }
+
+        if($(elementtoclick).attr('class') == "key caps") {
+          if (caps == "off") {
+            caps = "on";
+          }
+          else {
+            caps = "off";
+          }
+          previousClass = "key caps";
+        }
+
+        if($(elementtoclick).attr('class') == "key backspace") {
+          var text = $(recentInputArea).val();
+          if (text.length > 1) {
+            text = text.slice(0, text.length - 1);
+          }
+          else {
+            text = "";
+          }
+          $(recentInputArea).val(text);
+          previousClass = "key backspace";
+        }
+
+        if($(elementtoclick).attr('class') == "key num dual") {
+          var num_dial = $.trim($(elementtoclick).text());
+          if(previousClass != "key shift left") {
+            var num = num_dial.slice(1,2)
+            $(recentInputArea).val($(recentInputArea).val() + num);
+          }
+          else {
+            var dial = num_dial.slice(0,1);
+            $(recentInputArea).val($(recentInputArea).val() + dial);
+          }
+          previousClass = "key num dual";
+        }
+
+        if ($(elementtoclick).attr('class') == "key shift left") {
+          previousClass = "key shift left";
+        }
+      }
+      cont +=1;
+    });
+
+    $("#horizontal-scanbar").hide();
+    $("#vertical-scanbar").hide();
+  }
 }
 
 //MAGNIFIER-------------------------
